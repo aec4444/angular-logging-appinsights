86fd72b5189173894b05f467a69604c50a1bb4bc
Anthony Cornetto <anthony.cornetto@gaf.com>
2019-03-27 11:46:58 -0400

    updates to our process

--- copy-version-libs.js
+++ copy-version-libs.js
@@ -5,6 +5,7 @@ var jsonObject = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 // get the version #
 var version = jsonObject.version;
+console.log('Current Version: ' + version);
 
 // now copy to the libraries
 var librariesToCopy = [
@@ -15,7 +16,9 @@ for (var i = 0; i < librariesToCopy.length; i++) {
   var library = librariesToCopy[i];
 
   var libraryJsonObject = JSON.parse(fs.readFileSync(library, 'utf8'));
-  librariesToCopy.version = version;
+  libraryJsonObject.version = version;
   var libraryJsonString = JSON.stringify(libraryJsonObject, null, 2);
+
+  console.log('Updated ' + library + ' to ' + version);
   fs.writeFileSync(library, libraryJsonString);
 }
